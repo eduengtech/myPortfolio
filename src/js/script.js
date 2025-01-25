9a6e906d34395ffc3e68c93fd9ceba9d1d3e256a
Eduardo Victor <eduardodejesus.dev@gmail.com>
2025-01-24 22:53:05 -0300

    refactor: adding links to social networks

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', () =>{
         mobileMenu.classList.toggle("mobile-btn-open");
     });
 });
+/* Scroll */
+
+function scrollToSection(sectionId){
+    const section = document.querySelector(sectionId);
+
+    if(section){
+        let scrollOffset = 0;
+
+        if(sectionId === "#projects"){
+            scrollOffset = section.offsetTop - 70;
+        } else {
+            scrollOffset = section.offsetTop - (window.innerHeight - section.clientHeight) / 2;
+        }
+
+        window.scrollTo({
+            top: scrollOffset,
+            behavior: 'smooth'
+        })
